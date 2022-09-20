56edf5c4c50224ff5dbb17b4c4d25fa2be565bf3
Joongseok Jeon <jeonjungseok1203@gmail.com>
2022-09-20 14:37:52 +0900

    [ADD] connections_port.sh

---
+++ IT_Operations/connections_port.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+grep -v "rem_address" /proc/net/tcp  | awk 'function hextodec(str,ret,n,i,k,c){
+    ret = 0
+    n = length(str)
+    for (i = 1; i <= n; i++) {
+        c = tolower(substr(str, i, 1))
+        k = index("123456789abcdef", c)
+        ret = ret * 16 + k
+    }
+    return ret
+} {x=hextodec(substr($2,index($2,":")-2,2)); for (i=5; i>0; i-=2) x = x"."hextodec(substr($2,i,2))}{print x":"hextodec(substr($2,index($2,":")+1,4))}' | sort | uniq -c | sort -rn
